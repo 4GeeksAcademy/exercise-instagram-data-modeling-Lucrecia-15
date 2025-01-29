1ebbe7f07484bbd12f77f9bb3601c05dbd1e0798
Lucrecia-15 <parodilucrecia@gmail.com>
2025-01-29 19:54:01 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,67 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, unique=True)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    age = Column(String(250))
+    followed = Column(String(250), nullable=False)
+    followers = Column(String(250), nullable=False)
+    email = Column(String(250), unique=True, nullable=False)
+    phone = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class business_Account(Base):
+    __tablename__= 'business'
+    id = Column(Integer, primary_key=True, unique=True)
+    user_business = Column(String(250), nullable=False)
+    bussiness_email = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id = Column(Integer, primary_key=True, unique=True)
+    tag = Column(String(250))
+    post = Column(String(250), nullable=False)
+    likes = Column(String(250))
+    comments = Column(String(250))
+    location = Column(String(250))
+    data =  Column(String(250), nullable=False)
+    status = Column(String(64), default='pending')
+    user_id = Column(Integer, ForeignKey('user.id'))
+    business_id = Column(Integer, ForeignKey('business.id'))
+    user = relationship(User)
+    user = relationship( business_Account)
+
+class Media(Base):
+    __tablename__ = 'media'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True, unique=True)
+    photo = Column(String(250))
+    video = Column(String(250))
+    reel = Column(String(250))
+    live = Column(String(250))
+    Comments = Column(String(250))
+    user_id = Column(Integer, ForeignKey('post.id'))
+    user = relationship(Post)
+
+class Comments(Base):
+    __tablename__ = 'comments'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True, unique=True)
+    data = Column(String(250))
+    user_id = Column(Integer, ForeignKey('comments.id'))
+    user_id = Column(Integer, ForeignKey('post.id'))
+    user = relationship(Post)
+
 
     def to_dict(self):
         return {}
